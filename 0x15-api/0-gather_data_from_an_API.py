7c49b2be4751b85ef039e51750c21d79ec4903da
anda <terbu102@gmail.com>
2023-10-23 09:59:49 -0700

    api url

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,14 @@
 #!/usr/bin/python3
-""" a script that print rest api"""
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if __name__ == '__main__':
-    def get_employee_todo_progress(employee_id):
-        """ a function that get request api"""
-        base_url = 'https://jsonplaceholder.typicode.com'
-        employee_url = f'{base_url}/users/{employee_id}'
-        todos_url = f'{base_url}/todos?userId={employee_id}'
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-        try:
-            employee_response = requests.get(employee_url)
-            employee_data = employee_response.json()
-
-            todos_response = requests.get(todos_url)
-            todos_data = todos_response.json()
-
-            total_tasks = len(todos_data)
-            completed_tasks = sum(1 for todo in todos_data if todo['completed'])
-
-            print(
-
-                f"Employee {employee_data['name']} is done with tasks
-                ({completed_tasks}/{total_tasks}): "
-
-                )
-
-            for todo in todos_data:
-                if todo['completed']:
-                    print(f"\t{todo['title']}")
-
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
-            sys.exit(1)
-
-    if len(sys.argv) != 2:
-        print("Usage: python script.py employee_id")
-            sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
