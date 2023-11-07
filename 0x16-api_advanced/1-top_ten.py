54613ce135e319d6eb4a2afe188f77039181b39a
anda <terbu102@gmail.com>
2023-11-07 06:12:08 -0800

    meta

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a script that print ten site"""
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'App'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        
+        for post in posts:
+            title = post['data']['title']
+            print(title)
+    else:
+        print(None)
